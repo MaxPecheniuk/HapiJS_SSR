e485eb2d6074b17758f5102511320b338a105b69
Maksym <pecheniuk.maksym@gmail.com>
2019-04-19 17:53:05 +0300

    work commit

--- src/server/qraphql/resolvers/resolver.js
+++ src/server/qraphql/resolvers/resolver.js
@@ -5,33 +5,37 @@ const postReducer = require('../reducers/postReducer');
 
 module.exports = {
   Query: {
+
     posts: (_, __, context) => {
-      const ids = [1, 2, 3];
+      // const ids = [1, 2, 3];
       //Лоадер вызываем промис для каждого ключа, который делает запрос.
+      // const loaderPosts = async (keys) => await Promise.all(keys.map((key) => axios.get(url.apiUrl).then((res) => res.data.posts)));
       const loaderPosts = async (keys) => await Promise.all(keys.map((key) => axios.get(url.apiUrl).then((res) => res.data.posts)));
       //создаем контекст
       let loader = context.loaderPosts;
       if (!loader) {
-
         //создаем даталоадер и передаем в лоадер ключи
         context.loaderPosts = loader = new DataLoader((key) => loaderPosts(key))
       }
+      return loader.load([])
 
-      return loader.load(ids)
+    },
+    comments: (ids, args, context) => {
+      console.log('ids');
 
+      console.log(ids);
+      console.log(args);
+      // const loadComments = async (keys) => await Promise.all(keys.map((key) => httpCLient.get(key)));
+      // let loader = context.commentLoader;
+      // if (!loader) {
+      //   context.commentLoader = loader = new DataLoader((keys) => loadComments(keys));
+      // }
+      //
+      // return loader.load(ids);
     }
   },
-  // Post: {
-  //   comments: (ids, args, context) => {
-  //
-  //     const loadComments = async (keys) => await Promise.all(keys.map((key) => httpCLient.get(key)));
-  //     let loader = context.commentLoader;
-  //     if (!loader) {
-  //       context.commentLoader = loader = new DataLoader((keys) => loadComments(keys));
-  //     }
-  //
-  //     return loader.load(ids);
-  //   }
-  // }
+
+
+
 
 };
